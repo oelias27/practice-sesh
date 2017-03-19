80112594de8daa6a3258c201ca65f73f34f53adf
Orlando Elias <oelias27@gmail.com>
2017-03-19 00:22:10 -0700

    Added activity graph shell using component state

--- src/components/Home/Activity/Activity.js
+++ src/components/Home/Activity/Activity.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import Cell from './Cell/Cell.js';
+
 import './Activity.css';
 
+
 export default class Activity extends Component {
+  constructor(props) {
+    super(props);
+
+    let generateHistory = () => {
+      let history = [];
+      let currentTime = Date.now();
+      let startingTime = currentTime - 32140800;
+
+      for (let i = 0; i < 371; i++) {
+        history.push(Math.floor(Math.random() * 8));
+      }
+
+      return history;
+    }
+
+    this.state = {
+      history: generateHistory()
+    }
+  }
 
   render() {
+
+    let graph = this.state.history.map((num, i) => {
+      return <Cell key={i} number={num} />
+    });
+
     return (
       <div>
-        <p>Activity</p>
+        <p>Activity test</p>
+        <div className="graph">
+          {graph}
+        </div>
       </div>
     )
   }
