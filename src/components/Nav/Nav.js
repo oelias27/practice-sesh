ef614ba6dd5a4d7f68a5c43ce72aa80c29401498
Orlando Elias <oelias27@gmail.com>
2017-03-24 11:00:34 -0700

    Styled login page. Added logout functionality

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router'
-
+import { browserHistory, Link } from 'react-router';
+  
 import './Nav.css';
 
 export default class Nav extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.logOut();
+    browserHistory.push('/login');
+  }
+
+  componentWillReceiveProps() {
+    console.log('receieved ')
+  }
+
   render () {
+    if (!this.props.authenticated) {
+      return false;
+    }
+
     return (
       <div className="nav">
         <div className="navButtonWrapper">
           <Link to="/home"><button>Home</button></Link>
           <Link to="/session/setup"><button>New</button></Link>
+          <button onClick={this.handleClick}>Logout</button>
         </div>
       </div>
     )
